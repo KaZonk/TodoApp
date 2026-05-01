973b7b0e112ade146a83c7c89734d4ca7626252a
KaZonk <daok@stu.otc.school.nz>
2026-05-01 12:10:09 +1200

    Put the items in a class and put up some frame work for the app

--- main.py
+++ main.py
@@ -4,29 +4,61 @@ from tkinter import ttk, messagebox, filedialog
 import csv
 from datetime import datetime, date
 
+class Todo_app:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("ToDoMate")
+        self.root.geometry("900x550")
+        self.tabs = ttk.Notebook(root)
+        dashboard_tab = ttk.Frame(self.tabs)
+        todo_tab = ttk.Frame(self.tabs)
+        timer_tab = ttk.Frame(self.tabs)
 
-root = tk.Tk()
-tabs = ttk.Notebook(root)
-dashboard_tab = ttk.Frame(tabs)
-todo_tab = ttk.Frame(tabs)
-timer_tab = ttk.Frame(tabs)
+        self.tasks = []
 
-tabs.add(dashboard_tab, text="Dashboard")
-tabs.add(todo_tab, text="To-Do List")
-tabs.add(timer_tab, text="Pomodore Timer")
-tabs.pack(expand=True, fill="both")
+        self.tabs.add(dashboard_tab, text="Dashboard")
+        self.tabs.add(todo_tab, text="To-Do List")
+        self.tabs.add(timer_tab, text="Pomodoro Timer")
+        self.tabs.pack(expand=True, fill="both")
 
+        #Dash Board
+        ttk.Label(
+                    dashboard_tab,
+                    text="📊 ToDoMate Dashboard",
+                    font=("Segoe UI", 18, "bold")
+                ).pack(pady=50)
+        ttk.Label(
+                    dashboard_tab,
+                    text="Organise tasks • Manage priorities • Export data",
+                    font=("Segoe UI", 12)
+                ).pack()
+        
+        #Tasks List
 
-ttk.Label(
-            dashboard_tab,
-            text="📊 ToDoMate Dashboard",
-            font=("Segoe UI", 18, "bold")
-        ).pack(pady=50)
+        #Pomodoro Timer
 
-ttk.Label(
-            dashboard_tab,
-            text="Organise tasks • Manage priorities • Export data",
-            font=("Segoe UI", 12)
-        ).pack()
+    def add(self):
+        pass
+    
+    def remove(self):
+        pass
 
-root.mainloop()
+    def clear_all(self):
+        pass
+
+    def load(self):
+        pass
+
+    def export(self):
+        pass
+
+    def search(self):
+        pass
+
+    def sort(self):
+        pass
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Todo_app(root)
+    root.mainloop()
